5a6ddadfefb8bbec2e2d5d58972628fb119fb08c
Quoc PHAN <phquoc25@gmail.com>
2018-06-20 00:32:17 +0700

    Using @Input to pass parameter to a component

--- src/components/match-list/match-list.ts
+++ src/components/match-list/match-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Team } from '../../models/team';
 import { Match } from '../../models/match';
 
@@ -14,14 +14,18 @@ import { Match } from '../../models/match';
 })
 export class MatchListComponent {
 
-  matches: Match[];
+  @Input('matches') matches;
+  // matches: Match[];
 
   constructor() {
-    const currentTime = new Date();
-    const teamA: Team = new Team('a', 'a');
-    const match1 = new Match(currentTime, new Team("a", 'Germany'), new Team("a", 'Mexico'), 0, 1)
-    const match2 = new Match(currentTime, new Team("a", 'Portugal'), new Team("a", 'Spain'), 3, 3);
-    this.matches = [match1, match2];
+    // const currentTime = new Date();
+    // const teamA: Team = new Team('a', 'a');
+    // const match1 = new Match(currentTime, new Team("a", 'Germany'), new Team("a", 'Mexico'), 0, 1);
+    // const match2 = new Match(currentTime, new Team("a", 'Portugal'), new Team("a", 'Spain'), 3, 3);
+    // this.matches = [match1, match2];
   }
 
+  matchSelected(match: Match) {
+
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,11 +11,11 @@ export class HomePage {
 
   matches: any;
   constructor(public navCtrl: NavController) {
-    const currentTime = new Date();
+     const currentTime = new Date();
     const teamA: Team = new Team('a', 'a');
-    const match1 = new Match(currentTime, new Team("a", 'Germany'), new Team("a", 'Mexico'), null, null)
-    const match2 = new Match(currentTime, new Team("a", 'Portugal'), new Team("a", 'Spain'), null, null);
-    this.matches = 'matches';
+    const match1 = new Match(currentTime, new Team("a", 'Germany'), new Team("a", 'Mexico'), 0, 1);
+    const match2 = new Match(currentTime, new Team("a", 'Portugal'), new Team("a", 'Spain'), 3, 3);
+    this.matches = [match1, match2];
   }
 
 }
